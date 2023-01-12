12c4ebe94308a94649922e2b20df13b1198b4e99
Shadowstrikesblu <hmoulk8@gmail.com>
2023-01-12 15:23:37 +0100

    12/01

--- src/main/java/com/wymee/backparser/parser_backend_api/Main.java
+++ src/main/java/com/wymee/backparser/parser_backend_api/Main.java
@@ -1,13 +1,26 @@
 package com.wymee.backparser.parser_backend_api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
 
 }
